6d7da5a65af5e6e78df6bca273a258caf0a92f2e
Nilton Cezar <ncadeveloper@outlook.com>
2021-01-19 23:23:39 -0300

    Criando pagina detalhes

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,8 +1,6 @@
 export default {
     USER: '/users/$name',
     USER_REPOS: '/users/$name/repos',
-    ORG: '/orgs/$name',
-    ORG_REPOS: '/orgs/$name/repos',
     SEARCH_USERS: '/search/users?q=$query',
     SEARCH_REPOSITORIES: '/search/repositories?q=$query'
 };
--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,36 +1,66 @@
 import './index.css';
 import Routes from '../../constants/api';
 
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import { Api } from '../../services';
 import { Header, Footer, Breadcrumb } from '../../helpers';
 
 export const Details = () => {
     const { repo } = useParams();
+    const [ details, setDetails ] = useState({});
+    const [ repos, setRepos ] = useState([]);
 
     const _getDetails = () => {
         const api = new Api(Routes.USER.replace('$name', repo), 'GET');
         api.exec()
-            .then(({ data }) => {
-                console.log(data);
-            })
-            .finally(() => {
-                
-            });
+            .then(({ data }) => setDetails(data));
+    }
+
+    const _getRepos = () => {
+        const api = new Api(Routes.USER_REPOS.replace('$name', repo), 'GET');
+        api.exec()
+            .then(({ data }) => setRepos(data));
     }
 
     useEffect(() => {
+        _getRepos();
         _getDetails();
     }, []);
 
+    console.log(details, repos);
     return(
         <>
             <Header dark />
             <section id="details">
                 <Breadcrumb page="Details" />
-                
+                <div className="profile">
+                    <img className="profile__image" src={details?.avatar_url} alt="Profile"/>
+                    <div className="profile__info">
+                        <p>{details?.name}</p>
+                        <p><strong>Bio: </strong>{details?.bio ?? '-'}</p>
+                        <p><strong>Blog: </strong>{details?.blog ?? '-'}</p>
+                    </div>
+                    <div className="profile__info">
+                        <p>Have {details?.followers ?? 0} followers</p>
+                        <p><strong>Company: </strong>{details?.company ?? '-'}</p>
+                        <p><strong>Location: </strong>{details?.location ?? '-'}</p>
+                    </div>
+                </div>
+                <div className="repos">
+                    {repos.map(repo => 
+                        <div 
+                            key={repos.id} 
+                            className="card"
+                        >
+                            <h3 className="title">{repo.name}</h3>
+                            <p className="desc">{repo?.description ?? '-'}</p>
+                            <p className="desc">{repo.stargazers_count} stars</p>
+                            <a className="link" href={repo.html_url} target="blank">Ver no github</a>
+                        </div>
+                    )}
+                </div>
             </section>
             <Footer/>
         </>
